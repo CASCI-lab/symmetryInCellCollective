fb10a5b55668734ff2b46a261eed8a83d3416eb5
Austin Marcus <bwvxnt@gmail.com>
2023-05-23 11:33:38 -0400

    added timeout to speedup plot

--- data_analysis.py
+++ data_analysis.py
@@ -45,39 +45,74 @@ def analysis(data, source):
 	print(numTimeoutsNew, numTimeoutsOld)
 
 	# make two column figure
-	fig, axes = plt.subplots(1,2, width_ratios=[0.8, 0.2], figsize=(10,10))
+	# fig, axes = plt.subplots(1,2, width_ratios=[0.8, 0.2], figsize=(10,10))
+	fig = plt.figure(figsize=(10,10))
 
 	# plot old vs new time and if it was correct or not
+	ax1 = plt.subplot2grid((6,7), (1,0), colspan=3, rowspan=5)
 	colors = ["blue" if x==True else "red" for x in dataUniq[dataUniq["version"]=="v0.1.2"]["correct"]]
-	axes[0].scatter(dataUniq[dataUniq["version"]=="new"]["time_sym"], dataUniq[dataUniq["version"]=="v0.1.2"]["time_sym"], alpha=0.15, s=dataUniq[dataUniq["version"]=="new"]["k"]**2*5, c=colors)
-	axes[0].plot(np.arange(0, 2, 0.1), np.arange(0,2,0.1), linestyle="dashed", color="black")
+	ax1.scatter(dataUniq[dataUniq["version"]=="new"]["time_sym"], dataUniq[dataUniq["version"]=="v0.1.2"]["time_sym"], alpha=0.15, s=dataUniq[dataUniq["version"]=="new"]["k"]**2*5, c=colors)
+	ax1.plot(np.arange(0, 2, 0.1), np.arange(0,2,0.1), linestyle="dashed", color="black")
 	# plt.xlim([0, 2]); plt.ylim([0,2])
-	axes[0].set_xlim([10**-4, 10])
-	axes[0].set_ylim([10**-4, 10])
-	axes[0].set_yscale("log"); axes[0].set_xscale("log")
-	axes[0].set_xlabel("TSS time, schematodes (sec)", fontsize=22)
-	axes[0].set_ylabel("TSS time, v0.1.2 (sec)", fontsize=22)
-	axes[0].tick_params(axis="both", labelsize=16)
+	ax1.set_xlim([10**-4, 10])
+	ax1.set_ylim([10**-4, 10])
+	ax1.set_yscale("log"); ax1.set_xscale("log")
+	ax1.set_xlabel("TSS time, schematodes (sec)", fontsize=22)
+	ax1.set_ylabel("TSS time, v0.1.2 (sec)", fontsize=22)
+	ax1.tick_params(axis="both", labelsize=16)
 	redCirc = mlines.Line2D([], [], color="red", marker="o", linestyle="None", markersize=10, label="incorrect in v0.1.2")
 	blueCirc = mlines.Line2D([], [], color="blue", marker="o", linestyle="None", markersize=10, label="correct in v0.1.2")
-	axes[0].legend(handles=[redCirc, blueCirc])
+	ax1.legend(handles=[redCirc, blueCirc])
+	ax1.margins(0)
 
 	# plot boxplot of speedup between old and new, per function
 	# compute speedup per function
+	ax2 = plt.subplot2grid((6,7), (0,5), rowspan=6, colspan=2)
 	speedups = [x/y for x,y in zip(list(dataUniq[dataUniq["version"]=="v0.1.2"]["time_sym"]), list(dataUniq[dataUniq["version"]=="new"]["time_sym"]))]
 	print("mean speedup:",np.nanmean(speedups))
-	axes[1].boxplot([np.log10(x) for x in speedups if not np.isnan(x)])
-	# axes[1].set_yscale("log")
-	axes[1].set_ylabel("$\log_{10}($Times Speedup$)$", fontsize=22)
-	# axes[1].set_xlabel("v0.1.2 / schematodes")
-	axes[1].set_xticks([])
-	axes[1].tick_params(axis="both", labelsize=16)
-	axes[1].axhline(np.log10(np.nanmean(speedups)), linestyle="dashed", c="black", label="mean")
-	axes[1].set_ylim([-4,4])
-	plt.legend()
+	ax2.boxplot([np.log10(x) for x in speedups if not np.isnan(x)])
+	# ax2.set_yscale("log")
+	ax2.set_ylabel("$\log_{10}($Times Speedup$)$", fontsize=22)
+	# ax2.set_xlabel("v0.1.2 / schematodes")
+	ax2.set_xticks([])
+	ax2.tick_params(axis="both", labelsize=16)
+	ax2.axhline(np.log10(np.nanmean(speedups)), linestyle="dashed", c="black", label="mean")
+	ax2.set_ylim([-4,4])
+
+	# plot timeouts in separate axes
+	dataUniqMerge = dataUniq[dataUniq["version"]=="new"].merge(dataUniq[dataUniq["version"]=="v0.1.2"], on="Unnamed: 0")
+	newOnlyTimeout = dataUniqMerge[(dataUniqMerge["timeout_x"]==True)&(dataUniqMerge["timeout_y"]==False)]
+	newOnlyTimeout.to_csv(f"newOnlyTimeout-{source}.csv")
+	oldOnlyTimeout = dataUniqMerge[(dataUniqMerge["timeout_x"]==False)&(dataUniqMerge["timeout_y"]==True)]
+	# how many timeout on both?
+	print("both timeout:", len(dataUniqMerge[(dataUniqMerge["timeout_x"]==True)&(dataUniqMerge["timeout_y"]==True)]))
+
+	ax3 = plt.subplot2grid((6,7), (0,0), colspan=3)
+	# colors = ["blue" if x==True else "red" for x in oldOnlyTimeout["correct_
+	ax3.scatter(oldOnlyTimeout["time_sym_x"], [0]*len(oldOnlyTimeout.index), c="black", alpha=0.2, s=oldOnlyTimeout["k_x"]**2*5)
+	ax3.set_yticks([])
+	ax3.set_xlim([10**-4, 10])
+	ax3.set_xscale("log")
+	ax3.set_xticks([], minor=True)
+	ax3.set_xticks([], minor=False)
+	ax3.set_ylabel("timeout")
+
+	ax4 = plt.subplot2grid((6,7), (1,3), rowspan=5)
+	colors = ["blue" if x==True else "red" for x in newOnlyTimeout["correct_y"]]
+	ax4.scatter([0]*len(newOnlyTimeout.index), newOnlyTimeout["time_sym_y"], c=colors, alpha=0.2, s=newOnlyTimeout["k_x"]**2*5)
+	ax4.set_xticks([])
+	ax4.set_ylim([10**-4, 10])
+	ax4.set_yscale("log")
+	ax4.set_yticks([], minor=True)
+	ax4.set_yticks([], minor=False)
+	ax4.set_xlabel("timeout")
+	ax4.margins(0)
+
+	# plt.legend()
 	plt.tight_layout()
 	fig.savefig(f"figs/speedup-{source}.pdf")
 
+	
 	# plot ks with NON-unique functions
 	fig, axes = plt.subplots(2,1, figsize=(5,5))
 	sns.set_context("paper")
